2dccf2c8b3fc639b07b0478250c3be9354c4afdc
hsingh <hsingh@bosaproperties.com>
2024-12-03 12:33:10 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import CocktailList from "./components/page";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <CocktailList />
+      </div>
+    </>
+  );
+}
+
+export default App;
